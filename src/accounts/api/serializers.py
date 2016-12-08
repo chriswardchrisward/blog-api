1559eb89e679f945fc46e8adede1e00c4ee77392
Chris Ward <chriswardchrisward@gmail.com>
2016-12-07 23:00:31 -0800

    added UserLoginSerializer validation

--- src/accounts/api/serializers.py
+++ src/accounts/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 
 from rest_framework.serializers import (
 	CharField,
@@ -64,8 +65,8 @@ class UserCreateSerializer(ModelSerializer):
 
 class UserLoginSerializer(ModelSerializer):
 	token = CharField(allow_blank=True, read_only=True)
-	username = CharField()
-	email = EmailField(label='Email Address')
+	username = CharField(required=False, allow_blank=True)
+	email = EmailField(label='Email Address', required=False, allow_blank=True)
 	class Meta:
 		model = User
 		fields = [
@@ -79,8 +80,27 @@ class UserLoginSerializer(ModelSerializer):
 					}
 
 	def validate(self, data):
-		# email = data['email']
-		# user_qs = User.objects.filer(email=email)
-		# if user_qs.exists():
-		# 	raise ValidationError("This user has already registered.")
+		user_obj = None
+		email = data.get("email", None)
+		username = data.get("username", None)
+		password = data['password']
+		if not email and not username:
+			raise ValidationError("A username or email is required to login")	
+
+		user = User.objects.filter(
+				Q(email=email) |
+				Q(username=username)
+			).distinct()
+		user = user.exclude(email__isnull=True).exclude(email__iexact='')
+		if user.exists() and user.count() == 1:
+			user_obj = user.first()
+		else:
+			raise ValidationError("This username/email is not valid.")
+
+		if user_obj:
+			if not user_obj.check_password(password):
+				raise ValidationError("Incorrect credentials please try again")
+
+		data["token"] = "Some Random Token"
+
 		return data
