1218fb36eab43636e88f4e063b2335dd3e9369e5
Chris Ward <chriswardchrisward@gmail.com>
2016-12-07 21:35:12 -0800

    Started Base APIView for User Login no auth yet

--- src/accounts/api/serializers.py
+++ src/accounts/api/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
 
 from rest_framework.serializers import (
+	CharField,
+	EmailField,
 	HyperlinkedIdentityField,
 	ModelSerializer,
 	SerializerMethodField,
@@ -11,16 +13,43 @@ from rest_framework.serializers import (
 User = get_user_model()
 
 class UserCreateSerializer(ModelSerializer):
+	email = EmailField(label='Email Address')
+	email2 = EmailField(label='Confirm Email')
 	class Meta:
 		model = User
 		fields = [
 			'username',
 			'email',
+			'email2',
 			'password',
 		]
 		extra_kwargs = {"password":
 					{"write_only": True}
 					}
+
+	def validate(self, data):
+		# email = data['email']
+		# user_qs = User.objects.filer(email=email)
+		# if user_qs.exists():
+		# 	raise ValidationError("This user has already registered.")
+		return data
+
+	def validate_email(self, value):
+		data = self.get_initial()
+		email1 = data.get("email2")
+		email2 = value
+		if email1 != email2:
+			raise ValidationError("Emails must match")
+		return value
+
+	def validate_email2(self, value):
+		data = self.get_initial()
+		email1 = data.get("email")
+		email2 = value
+		if email1 != email2:
+			raise ValidationError("Emails must match")
+		return value
+
 	def create(self, validated_data):
 		username = validated_data['username']
 		email = validated_data['email']
@@ -32,3 +61,26 @@ class UserCreateSerializer(ModelSerializer):
 		user_obj.set_password(password)
 		user_obj.save()
 		return validated_data
+
+class UserLoginSerializer(ModelSerializer):
+	token = CharField(allow_blank=True, read_only=True)
+	username = CharField()
+	email = EmailField(label='Email Address')
+	class Meta:
+		model = User
+		fields = [
+			'username',
+			'email',
+			'password',
+			'token'
+		]
+		extra_kwargs = {"password":
+					{"write_only": True}
+					}
+
+	def validate(self, data):
+		# email = data['email']
+		# user_qs = User.objects.filer(email=email)
+		# if user_qs.exists():
+		# 	raise ValidationError("This user has already registered.")
+		return data
